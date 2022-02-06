cbf6deda6bea528801c7f4634966dce12b5cf0cc
julien <contact@julien-vital.dev>
2022-02-06 15:48:48 +0100

    todolist controller

---
+++ src/Controller/TodoListController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Pimcore\Controller\FrontendController;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class TodoListController extends FrontendController
+{   /**
+    * @Template
+    * @param Request $request
+    * @return array
+    */
+    public function defaultAction(Request $request)
+    {
+        return [];
+    }
+
+    public function todoListAction(Request $request)
+    {
+
+        return $this->render('todolist/default.html.twig');
+    }
+}
